4c5d919dbca3ca11204dacdb596e21562bc9b237
Vamshi Alankar <vamshi.alankar@gmail.com>
2025-05-19 10:41:58 -0500

    Create main.tf

---
+++ Terraform/main.tf
@@ -0,0 +1,17 @@
+# Sample of my Terraform and Provider Block
+
+# Terraform Block
+terraform {
+  required_version = ">= 1.7" 
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.33"
+    }
+  }
+}
+
+# Provider Block
+provider "aws" {
+  region  = var.aws_region
+}
